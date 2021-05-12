76f06261b2ca38400379371853a38a617d99b133
Sefa <sefagokceoglu06@gmail.com>
2021-05-12 15:47:31 +0300

    Deleted unnecessary code

--- static/spa/src/App.js
+++ static/spa/src/App.js
@@ -131,16 +131,6 @@ function App() {
             change.created.split("T")[1].split(".")[0],
         });
       }
-      // @ts-ignore
-      // if (countryInfo.name) {
-      //   payload.options.push({
-      //     // @ts-ignore
-      //     value: change.author.displayName +
-      //   });
-      // } else {
-      //   // @ts-ignore
-      //   throw new Error(`No name found for ID: ${countryInfo.id}`);
-      // }
     }
 
     if (payload.options[0] != undefined) {
@@ -157,43 +147,6 @@ function App() {
       console.log("Add country field options response:", await response.json());
     }
   };
-
-  // const createCustomField = async () => {
-  //   const payload = {
-  //     searcherKey:
-  //       "com.atlassian.jira.plugin.system.customfieldtypes:multiselectsearcher",
-  //     name: "Change Logs",
-  //     description: "Custom field for Change Logs",
-  //     type: "com.atlassian.jira.plugin.system.customfieldtypes:select",
-  //   };
-  //   const create_options = {
-  //     method: "POST",
-  //     body: JSON.stringify(payload),
-  //   };
-  //   const response_from_create = await api
-  //     .asUser()
-  //     .requestJira(`/rest/api/3/field`, create_options);
-  //   console.log("Create country field response:", response_from_create);
-  //   const ChangeLogsFieldData = await response_from_create.json();
-
-  //   const fieldId = ChangeLogsFieldData.schema.customId;
-
-  //   const add_default_options = {
-  //     method: "POST",
-  //   };
-  //   const response = await api
-  //     .asUser()
-  //     .requestJira(
-  //       `/rest/api/3/screens/addToDefault/customfield_${fieldId}`,
-  //       add_default_options
-  //     );
-  //   console.log(
-  //     "Add Custom Change Log field to default screen response:",
-  //     response
-  //   );
-  //   const responseData = await response.json();
-  //   console.log(" * responseData:", responseData);
-  // };
 
   return (
     <IssuePanel
